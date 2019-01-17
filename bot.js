ce7a13ca7ca31e2807f2abac54227b1f856152f2
yahyaaaz <46232918+yahyaaaz@users.noreply.github.com>
2019-01-17 07:41:04 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,29 +2,29 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = "d";
 var adminprefix = '&'
-const developers = ['499540032277708824','490252038492389379'] //الاي دى حقك
+const developers = ['455895026547818537','529646570983915551'] //الاي دى حقك
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
      
   if (message.content.startsWith(adminprefix + 'setg')) {
     client.user.setGame(argresult);
-      message.channel.send(`BY !RG , ZEUS#9699   ${argresult}**`)
+      message.channel.send(`Y A H Y A #9952   ${argresult}**`)
   } else
      if (message.content === (adminprefix + "leave")) {
     message.guild.leave();        
   } else  
   if (message.content.startsWith(adminprefix + 'setw')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(`BY !RG , ZEUS#9699   ${argresult}**`)
+      message.channel.send(`Y A H Y A #9952   ${argresult}**`)
   } else
   if (message.content.startsWith(adminprefix + 'setl')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`BY !RG , ZEUS#9699   ${argresult}**`)
+      message.channel.send(`Y A H Y A #9952   ${argresult}**`)
   } else
   if (message.content.startsWith(adminprefix + 'sets')) {
     client.user.setGame(argresult, "https://www.twitch.tv/One");
-      message.channel.send(`BY !RG , ZEUS#9699`)
+      message.channel.send(`Y A H Y A #9952`)
   }
   if (message.content.startsWith(adminprefix + 'setname')) {
   client.user.setUsername(argresult).then
